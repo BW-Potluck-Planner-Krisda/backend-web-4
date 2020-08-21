4cc752ca5b0d48445d30a0ec9ed2d294715bbda2
marie-hotchkiss <mtknowlton67@gmail.com>
2020-08-21 16:54:31 -0700

    Re-Do code

--- api/server.js
+++ api/server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 
+const authenticate = require('../users/authenticate-middleware.js');
+const authRouter = require('../users/user-router.js');
+const potluckRouter = require('../potlucks/potluck-router');
+const potluckUserRouter = require('../users/potluck-user-router');
+const foodsRouter = require('../food/food-router');
 
 const server = express();
 
@@ -12,3 +17,11 @@ server.use(express.json());
 server.get('/', (req, res) => {
     res.status(200).json({ message: 'server up and running'});
 });
+
+server.use('/api/', authRouter);
+server.use(authenticate);
+server.use('/potlucks', potluckRouter);
+server.use('/users', potluckUserRouter);
+server.use('/food', foodsRouter);
+
+module.exports = server;
